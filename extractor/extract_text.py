dc8e6094552c5541709a0060006ba115ee20c4d8
Niarfe <efrain.olivares@gmail.com>
2019-08-24 03:24:27 -0700

    add extractor to retrieve text from pdfs

---
+++ extractor/extract_text.py
@@ -0,0 +1,35 @@
+"""
+Extract text from a pdf.  Pass in the name of a file and text content
+will be sent to console
+"""
+import PyPDF2
+import json
+import re
+
+def extract_text(filename):
+    
+    pdfReader = PyPDF2.PdfFileReader(open(filename, 'rb'))
+    return {idx:pageObj.extractText() for idx,  pageObj in enumerate([pdfReader.getPage(pagenum) for pagenum in range(pdfReader.numPages)])}
+
+def flatten_text(page_text):
+    return re.sub(r'\s+', ' ', page_text)
+
+
+def write_as_json_file(d_pages, filename):
+    with open(filename.replace('.pdf', '.json'), 'w') as target:
+        for page, text in d_pages.items():
+            target.write(json.dumps({page:text}))
+            target.write("\n")
+
+if __name__ == "__main__":
+    import argparse as arg
+    parser = arg.ArgumentParser(description='Pdf text extractor')
+    parser.add_argument('--source', dest="source", type=str, required=True)
+
+    args = parser.parse_args()
+    pages = extract_text(args.source)
+
+    write_as_json_file(pages, args.source)
+    print("Done converting {}".format(args.source))
+
+
