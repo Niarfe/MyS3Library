274017f361e5d3aa9a535187a24a075082c31f44
EfrainOlivaresCD <efrain.olivares@carpe.io>
2019-08-24 19:08:42 -0700

    updating cleaning up extractor script

--- extractor/extract_text.py
+++ extractor/extract_text.py
@@ -7,7 +7,6 @@ import json
 import re
 
 def extract_text(filename):
-    
     pdfReader = PyPDF2.PdfFileReader(open(filename, 'rb'))
     return {idx:pageObj.extractText() for idx,  pageObj in enumerate([pdfReader.getPage(pagenum) for pagenum in range(pdfReader.numPages)])}
 
@@ -21,15 +20,18 @@ def write_as_json_file(d_pages, filename):
             target.write(json.dumps({page:text}))
             target.write("\n")
 
+def main(source_file_path):
+    pages = extract_text(source_file_path)
+    write_to_json_file(pages, source_file_path)
+
 if __name__ == "__main__":
     import argparse as arg
     parser = arg.ArgumentParser(description='Pdf text extractor')
     parser.add_argument('--source', dest="source", type=str, required=True)
-
     args = parser.parse_args()
-    pages = extract_text(args.source)
 
-    write_as_json_file(pages, args.source)
+    main(args.source)
+
     print("Done converting {}".format(args.source))
 
 
