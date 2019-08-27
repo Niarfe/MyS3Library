7d68f4ba04cdacf936dc0e4098574011f935b97f
EfrainOlivaresCD <efrain.olivares@carpe.io>
2019-08-26 22:50:02 -0700

    extractor check if we have file already

--- extractor/extract_text.py
+++ extractor/extract_text.py
@@ -1,28 +1,51 @@
 """
 Extract text from a pdf.  Pass in the name of a file and text content
 will be sent to console
+
+NOTE: looks like linux' pdftotxt does a better job
 """
-import PyPDF2
 import json
 import re
+import os
 
-def extract_text(filename):
-    pdfReader = PyPDF2.PdfFileReader(open(filename, 'rb'))
-    return {idx:pageObj.extractText() for idx,  pageObj in enumerate([pdfReader.getPage(pagenum) for pagenum in range(pdfReader.numPages)])}
 
 def flatten_text(page_text):
     return re.sub(r'\s+', ' ', page_text)
 
 
-def write_as_json_file(d_pages, filename):
+def write_to_json_file(d_pages, filename):
     with open(filename.replace('.pdf', '.json'), 'w') as target:
         for page, text in d_pages.items():
             target.write(json.dumps({page:text}))
             target.write("\n")
 
-def main(source_file_path):
-    pages = extract_text(source_file_path)
-    write_to_json_file(pages, source_file_path)
+
+def pdf_to_text(source_file_path):
+    cmd = "pdftotext {}".format(source_file_path)
+    os.system(cmd)
+    
+def main(file_list):
+    with open(file_list, 'r') as source:
+        for idx, fname_raw in enumerate(source):
+            fname = fname_raw.strip()
+            if not fname.endswith(".pdf"):
+                print("Non pdf, skipping: {}".format(fname))
+                continue
+            deja_vu = os.system("aws s3 ls s3://eolibrary/extractedbooks/{}".format(fname.replace('.pdf', '.txt')))
+            if deja_vu == 0:
+                print("\tDEJA_VU {}".format(fname))
+                continue
+            cmds = [
+                    "echo == STARTING # {} ===".format(idx),
+                    "aws s3 cp s3://eolibrary/books/{} .".format(fname),
+                    "pdftotext {}".format(fname),
+                    "aws s3 mv {}.txt s3://eolibrary/extractedbooks/".format(fname.replace(".pdf", "")),
+                    "rm {}".format(fname),
+                    ]
+            for cmd in cmds:
+                print(cmd)
+                os.system(cmd)
+            
 
 if __name__ == "__main__":
     import argparse as arg
