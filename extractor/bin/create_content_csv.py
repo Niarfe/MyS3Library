46e0642367e286dc53d1adfd275020929807c865
Niarfe <efrain.olivares@gmail.com>
2019-09-21 02:16:55 -0700

    clean up regex in create content a little

--- extractor/bin/create_content_csv.py
+++ extractor/bin/create_content_csv.py
@@ -9,7 +9,8 @@ with open('output/'+'content.csv', 'w') as target:
     target.write("id,name,content\n")
     for idx, fname in enumerate(fnames):
         text = open(content_dir+fname).read()
-        flat_lowercase_text = re.sub(r'\s+', ' ', text).lower().replace(',', ' ')
+        alpha_num = re.sub(r'\W+', ' ', text)
+        flat_lowercase_text = re.sub(r'\s+', ' ', alpha_num).lower()
         target.write("{},{},{}\n".format(idx, fname.replace('.txt', '.pdf'), flat_lowercase_text.strip().replace('\x00', '')))
         if idx % 100 == 0:
             print("done through ", idx)
