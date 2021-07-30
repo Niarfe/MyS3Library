cc8137ad9216efd8b7830edab0959371cb8275ed
EfrainOlivaresCD <efrain.olivares@carpe.io>
2021-07-30 03:47:44 -0700

    remove parenthesis and single quotes, commas on normalize

--- do/normalize_names.py
+++ do/normalize_names.py
@@ -1,9 +1,10 @@
 
 import os
+import re
 
 bs = 'bookshelf/'
 for fname in os.listdir('bookshelf/'):
     print(fname)
-    os.rename(bs+fname, bs+fname.replace(' ', '_'))
+    os.rename(bs+fname, bs+re.sub(r'[\',\(\)\s]', '_', fname))
 
 print("\nDone with normalization\n")
