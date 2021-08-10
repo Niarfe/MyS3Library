c593b5843bed17a302b201b9d611e31663e4f09b
EfrainOlivaresCD <efrain.olivares@carpe.io>
2021-08-10 03:11:09 -0700

    extract pdf image script fixes

---
+++ extract_pdf_images.py
@@ -0,0 +1,39 @@
+import fitz
+import io
+from PIL import Image
+
+# STEP 2
+# file path you want to extract images from
+fname = "BIEN_introduction.pdf"
+
+# open the file
+pdf_file = fitz.open(fname)
+
+# STEP 3
+# iterate over PDF pages
+for page_index in range(len(pdf_file)):
+    
+    # get the page itself
+    page = pdf_file[page_index]
+    image_list = page.getImageList()
+    
+    # printing number of images found in this page
+    if image_list:
+        print(f"[+] Found a total of {len(image_list)} images in page {page_index}")
+    else:
+        print("[!] No images found on page", page_index)
+
+    for image_index, img in enumerate(page.getImageList(), start=1):
+        
+        # get the XREF of the image
+        xref = img[0]
+        
+        # extract the image bytes
+        base_image = pdf_file.extractImage(xref)
+        image_bytes = base_image["image"]
+        
+        # get the image extension
+        image_ext = base_image["ext"]
+
+        xref.writePNG('blah/image_{}.png'.format(index_image))
+
